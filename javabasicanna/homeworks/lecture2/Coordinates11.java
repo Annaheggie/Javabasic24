f0912cd6ecb305c95ed653fe171e091d10248b7b
Anna Heggie <aheggiejr@gmail.com>
2024-02-08 00:36:25 -0800

    New file added

---
+++ javabasicanna/homeworks/lecture2/Coordinates11.java
@@ -0,0 +1,14 @@
+package javabasicanna.homeworks.lecture2;
+
+public class Coordinates11 {
+
+    public static void main(String[] args) {
+        System.out.println("Odesa Opera and Ballet Theatre " );
+        //Odesa Opera and Ballet Theatre
+        double latitude = 46.485556;
+        double longitude = 30.741667;
+
+        System.out.println("Latitude: " + latitude);
+        System.out.println("Longitude: " + longitude);
+    }
+}
