a54671578091cedc41912f8f16903024eb38356c
Anna Heggie <aheggiejr@gmail.com>
2024-02-11 10:38:30 -0800

    file added

---
+++ javabasicanna/homeworks/lecture7/RugbyTeam.java
@@ -0,0 +1,44 @@
+package javabasicanna.homeworks.lecture7;
+
+import java.util.Random;
+
+public class RugbyTeam {
+    public static void main(String[] args) {
+        int[] team1Ages = generateRandomAges(25);
+        int[] team2Ages = generateRandomAges(25);
+
+        System.out.println("Team 1 Ages:");
+        displayAges(team1Ages);
+        System.out.println("Average Age of Team 1: " + calculateAverageAge(team1Ages));
+
+        System.out.println("\nTeam 2 Ages:");
+        displayAges(team2Ages);
+        System.out.println("Average Age of Team 2: " + calculateAverageAge(team2Ages));
+    }
+
+    public static int[] generateRandomAges(int size) {
+        int[] ages = new int[size];
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            ages[i] = random.nextInt(23) + 18;
+        }
+
+        return ages;
+    }
+
+    public static void displayAges(int[] ages) {
+        for (int age : ages) {
+            System.out.print(age + " ");
+        }
+        System.out.println();
+    }
+
+    public static double calculateAverageAge(int[] ages) {
+        int sum = 0;
+        for (int age : ages) {
+            sum += age;
+        }
+        return (double) sum / ages.length;
+    }
+}
