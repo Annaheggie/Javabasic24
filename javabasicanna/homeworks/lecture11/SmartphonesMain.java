8850a0cdb57b96d95c36ca0323b42ed490a000c5
Anna Heggie <aheggiejr@gmail.com>
2024-02-25 21:46:40 -0800

    Change Class SmartphonesMain.java for lecture11

--- javabasicanna/homeworks/lecture11/SmartphonesMain.java
+++ javabasicanna/homeworks/lecture11/SmartphonesMain.java
@@ -1,4 +1,27 @@
 package javabasicanna.homeworks.lecture11;
 
+
 public class SmartphonesMain {
+    public static void main(String[] args) {
+        // Creating instances of Androids
+        Androids androidfirstdevice = new Androids();
+        Androids androidseconddevice = new Androids();
+        Androids androidthirddevice = new Androids();
+
+        // Creating instances of iPhones
+        iPhones iPhonefirstdevice = new iPhones();
+        iPhones iPhoneseconddevice = new iPhones();
+        iPhones iPhonethirddevice = new iPhones();
+
+        // Using the methods of Androids and iPhones
+        androidfirstdevice.call();
+        androidseconddevice.sms();
+        androidthirddevice.internet();
+        androidfirstdevice.runLinuxCommands();
+
+        iPhonefirstdevice.call();
+        iPhoneseconddevice.sms();
+        iPhonethirddevice.internet();
+        iPhonefirstdevice.faceTime();
+    }
 }
